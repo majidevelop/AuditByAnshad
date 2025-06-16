fb31613f920c0ee00e467ed750555600e0757569
Majid <musthafamajid@gmail.com>
2025-06-16 08:58:10 +0530

    tabls

---
+++ db/tables/forms_answer_versions.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 16, 2025 at 05:26 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ehse`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `forms_answer_versions`
+--
+
+CREATE TABLE `forms_answer_versions` (
+  `id` int(11) NOT NULL,
+  `audit_id` int(11) NOT NULL,
+  `question_id` int(11) NOT NULL,
+  `answer` text DEFAULT NULL,
+  `remarks` text DEFAULT NULL,
+  `submitted_by` int(11) NOT NULL,
+  `role` enum('team','lead','auditee') NOT NULL,
+  `stage` enum('audit_team','audit_lead','auditee') NOT NULL,
+  `version` int(11) NOT NULL,
+  `version_group_id` int(11) NOT NULL,
+  `is_latest` tinyint(1) DEFAULT 1,
+  `created_at` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `forms_answer_versions`
+--
+ALTER TABLE `forms_answer_versions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `forms_answer_versions`
+--
+ALTER TABLE `forms_answer_versions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
